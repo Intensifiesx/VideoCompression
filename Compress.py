d0e44e48f1b8b22839e562952da960f6fd890e21
Alex Akoopie <alexakoopie@gmail.com>
2024-05-09 20:40:38 -0700

    Updated Compress.py
    Changed video codec from VP9 to MPEG-4. Added input and output folders so the script can compress multiple videos at once without changing the path in the source code.

--- Compress.py
+++ Compress.py
@@ -6,26 +6,36 @@ def compressVidUsingFFMPEG(input_file, output_file):
     # Tried using H.264 codec but it was not as efficient as HEVC
     # Issue about VP9 is that it is slow to encode
     # Command to compress video using VP9 codec
+    # Alex: changed to mpeg4 which is not as slow as VP9, but some files get larger after compression
     cmd = [
         'ffmpeg',
         '-i', input_file,               # Input file
-        '-c:v', 'libvpx-vp9',           # VP9 codec
+        '-c:v', 'mpeg4',                # mpeg4 codec
         '-b:v', '1M',                   # Target bitrate for video
         '-c:a', 'libopus',              # Opus audio codec
         output_file                     # Output file
     ]
     subprocess.run(cmd)
 
-input = os.path.join(os.getcwd(), 'VIDEO.mp4')                  # CHANGE THIS TO YOUR INPUT FILE
-output = os.path.join(os.getcwd(), 'VIDEO_COMPRESSED.mp4')       # CHANGE THIS TO YOUR OUTPUT FILE
+input_path = os.path.join(os.getcwd(), 'Input')
+output_path = os.path.join(os.getcwd(), 'Output')
 
-inputSize = os.path.getsize(input) / (1024 * 1024)
+for directory in [input_path, output_path]:
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+        print(f"Directory '{directory}' created.")
 
-compressVidUsingFFMPEG(input, output)
+input = [os.path.join(input_path, file) for file in os.listdir(input_path)]
 
-outputSize = os.path.getsize(output) / (1024 * 1024)
+for video in input:
+    file_name = os.path.splitext(os.path.basename(video))[0]
+    inputSize = os.path.getsize(video) / (1024 * 1024)
 
-print(f"Input file size: {inputSize:.2f} MB")
-print(f"Output file size: {outputSize:.2f} MB")
-print(f"Compression ratio: {inputSize / outputSize:.2f}")
-input("Press Enter to continue...")
+    output = os.path.join(output_path, file_name + '_COMPRESSED.mp4')
+    compressVidUsingFFMPEG(video, output)
+
+    outputSize = os.path.getsize(output) / (1024 * 1024)
+
+    print(f"Input file size: {inputSize:.2f} MB")
+    print(f"Output file size: {outputSize:.2f} MB")
+    print(f"Compression ratio: {inputSize / outputSize:.2f}")
