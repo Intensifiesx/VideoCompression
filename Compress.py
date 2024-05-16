21161152363ebf646b53c4cc99d1d6a36b1bd4d0
Alex Akoopie <alexakoopie@gmail.com>
2024-05-15 21:09:17 -0700

    Added build capabilities using PyInstaller
    Changed the source code to support building binaries. Need ffmpeg/ folder in the main directory.

--- Compress.py
+++ Compress.py
@@ -1,5 +1,11 @@
 import os
 import subprocess
+import sys
+
+def resource_path():
+    if hasattr(sys, '_MEIPASS'):
+        return os.path.join(sys._MEIPASS, './ffmpeg/ffmpeg') # gets ffmpeg as a bundle (running executable)
+    return os.path.join(os.path.abspath("."), './ffmpeg/ffmpeg') # gets ffmpeg as a directory (running script)
 
 def compressVidUsingFFMPEG(input_file, output_file):
     # Tried using HEVC codec but it was not supported by other platforms
@@ -9,7 +15,7 @@ def compressVidUsingFFMPEG(input_file, output_file):
     # Alex: changed to mpeg4 which is not as slow as VP9, but some files get larger after compression
     # Alex: changed back to vp9 because mpeg4 barely compresses most of the time, but also encountered an issue with vp9 barely compressing for a 10 min video
     cmd = [
-        'ffmpeg',
+        resource_path(),
         '-i', input_file,               # Input file
         '-c:v', 'libvpx-vp9',           # vp9 codec
         '-b:v', '1M',                   # Target bitrate for video
@@ -18,25 +24,28 @@ def compressVidUsingFFMPEG(input_file, output_file):
     ]
     subprocess.run(cmd)
 
-input_path = os.path.join(os.getcwd(), 'Input')
-output_path = os.path.join(os.getcwd(), 'Output')
+# Execution Check
+if __name__ == "__main__":
+    input_path = os.path.join(os.getcwd(), 'Input')
+    output_path = os.path.join(os.getcwd(), 'Output')
 
-for directory in [input_path, output_path]:
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-        print(f"Directory '{directory}' created.")
+    # Creates input and output folders
+    for directory in [input_path, output_path]:
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+            print(f"Directory '{directory}' created.")
 
-input = [os.path.join(input_path, file) for file in os.listdir(input_path)]
+    input = [os.path.join(input_path, file) for file in os.listdir(input_path)]
 
-for video in input:
-    file_name = os.path.splitext(os.path.basename(video))[0]
-    inputSize = os.path.getsize(video) / (1024 * 1024)
+    for video in input:
+        file_name = os.path.splitext(os.path.basename(video))[0]
+        inputSize = os.path.getsize(video) / (1024 * 1024)
 
-    output = os.path.join(output_path, file_name + '_COMPRESSED.mp4')
-    compressVidUsingFFMPEG(video, output)
+        output = os.path.join(output_path, file_name + '_COMPRESSED.mp4')
+        compressVidUsingFFMPEG(video, output)
 
-    outputSize = os.path.getsize(output) / (1024 * 1024)
+        outputSize = os.path.getsize(output) / (1024 * 1024)
 
-    print(f"Input file size: {inputSize:.2f} MB")
-    print(f"Output file size: {outputSize:.2f} MB")
-    print(f"Compression ratio: {inputSize / outputSize:.2f}")
+        print(f"Input file size: {inputSize:.2f} MB")
+        print(f"Output file size: {outputSize:.2f} MB")
+        print(f"Compression ratio: {inputSize / outputSize:.2f}")
